405b9262d9d66d38e4291d803fb7e856dcf84286
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-01-22 13:55:26 +0300

    added function that return state for characters

--- src/services/gotService.js
+++ src/services/gotService.js
@@ -6,11 +6,11 @@ export default class GotService {
 
     async getResourse(url) {
         const res = await fetch(`${this._apiBase}${url}`);
-    
+
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
-    
+
         return await res.json();
     };
     getAllCharacters() {
@@ -32,5 +32,15 @@ export default class GotService {
     }
     getBook(id) {
         return this.getResourse(`/books/${id}`)
+    }
+
+    _transformCharacter(char) {
+        return {
+            name: char.name,
+            gender: char.gender,
+            born: char.born,
+            died: char.died,
+            culture: char.culture
+        }
     }
 };
