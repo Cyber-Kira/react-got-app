42295faa9fad63c6449197baf160c3e15f445d2a
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-01-22 13:15:45 +0300

    added api class service

---
+++ src/components/services/gotService.js
@@ -0,0 +1,22 @@
+export default class GotService {
+
+    constructor() {
+        this._apiBase = 'https://www.anapioficeandfire.com/api/';
+    }
+
+    async getResourse(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
+        return await res.json();
+    };
+    getAllCharacters() {
+        return this.getResourse(`/characters?page=5&pageSize=10`);
+    }
+    getCharacter(id) {
+        return this.getResourse(`/characters/${id}`)
+    }
+};
