0fb6294f4c48d7c6253d4018d3e0fbf4bcc44398
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-01-24 12:43:19 +0300

    now random character updates every 1.5 seconds, using lifecycle

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -6,17 +6,20 @@ import ErrorMessage from '../errorMessage/errorMessage';
 
 export default class RandomChar extends Component {
 
-    constructor() {
-        super();
-        
-        this.updateChar();
-    }
-
     GotService = new GotService();
     
     state = {
         char: {},
         loading: true
+    }
+
+    componentDidMount() {
+        this.updateChar();
+        this.timerId = setInterval(this.updateChar, 1500);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
     }
 
     onCharLoaded = (char) => {
@@ -34,7 +37,7 @@ export default class RandomChar extends Component {
         })
     }
 
-    updateChar() {
+    updateChar = () => {
         const id = Math.floor(Math.random() * 140 + 25); // 25-140
         
         this.GotService.getCharacter(id)
